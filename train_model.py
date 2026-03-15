4cd74ceebc85bd2babed3317a46d7f5f9c417874
bhumasupriya <110020186+bhumasupriya@users.noreply.github.com>
2026-03-15 17:31:30 +0530

    Add files via upload

---
+++ train_model.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+
+# Load dataset
+data = pd.read_csv("C:/Users/bhuma/Downloads/creditcard.csv")
+
+X = data.drop("Class", axis=1)
+y = data["Class"]
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X_scaled, y, test_size=0.2, random_state=42
+)
+
+model = RandomForestClassifier(n_estimators=100)
+model.fit(X_train, y_train)
+
+# Save
+pickle.dump(model, open("fraud_model.pkl", "wb"))
+pickle.dump(scaler, open("scaler.pkl", "wb"))
+
+print("Model trained and saved")
